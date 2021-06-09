8817c0f639ad2bf1573bcab9536f057479ea4a3d
Lukas Fruntke <lukas@fruntke.tech>
2021-06-09 16:42:54 +0200

    Fix Linter, refactor

--- src/components/contents/expandables/AbstractTextExpandable.jsx
+++ src/components/contents/expandables/AbstractTextExpandable.jsx
@@ -4,15 +4,14 @@ import withReactContent from 'sweetalert2-react-content';
 const MySwal = withReactContent(Swal);
 
 export const AbstractTextExpandable = props => {
-	if(!!props.titleDisplay){
-		props.titleDisplay= true;
+	if (props.titleDisplay) {
+		props.titleDisplay = true;
 	}
-	console.log(props.children)
+
 	return (
 		<div style={{cursor: 'pointer'}} onClick={() => textAlert(props.title, props.children[2], props.iconHtml, props.iconColor)}>
-			{props.titleDisplay ? <h4>{props.title}</h4> : <></>}
 			{props.children[0]}
-			{typeof props.children[1] === 'string' ? trunc(props.children[1], props.length) : props.children[1]}
+			{props.children[1]}
 		</div>
 	);
 };
@@ -33,17 +32,4 @@ export function textAlert(title, body, iconHtml = '⬤', iconColor = '#c79a00') 
 			popup: 'swal-wide'
 		}
 	});
-}
-
-function trunc(string, length = 500) {
-	const array = string.split('').splice(0, length);
-
-	if (string.length < length || array.lastIndexOf(' ') === -1) {
-		return string;
-	}
-
-	const space = array.lastIndexOf(' ');
-	const trim = array.splice(0, space);
-
-	return trim.join('') + ' [...]';
 }
--- src/components/contents/expandables/TextExpandable.jsx
+++ src/components/contents/expandables/TextExpandable.jsx
@@ -1,39 +1,21 @@
 import React from 'react';
-import Swal from 'sweetalert2';
-import withReactContent from 'sweetalert2-react-content';
-const MySwal = withReactContent(Swal);
+import {AbstractTextExpandable} from './AbstractTextExpandable.jsx';
 
 export const TextExpandable = props => {
-	if(!!props.titleDisplay){
-		props.titleDisplay= true;
-	}
 	return (
-		<div style={{cursor: 'pointer'}} onClick={() => textAlert(props.title, props.children, props.iconHtml, props.iconColor)}>
-			{props.titleDisplay ? <h4>{props.title}</h4> : <></>}
+
+		<AbstractTextExpandable>
+			<h4>{props.title}</h4>
 			<p>
 				{typeof props.children === 'string' ? trunc(props.children, props.length) : props.children}
 			</p>
-		</div>
+			<p>
+				{props.children}
+			</p>
+		</AbstractTextExpandable>
+
 	);
 };
-
-export function textAlert(title, body, iconHtml = '⬤', iconColor = '#c79a00') {
-	MySwal.fire({
-		title,
-		html: body,
-		iconColor,
-		iconHtml,
-		showCloseButton: true,
-		closeButtonAriaLabel: 'Info schliessen',
-		showCancelButton: false,
-		showConfirmButton: false,
-		focusConfirm: true,
-		customClass: {
-			// HtmlContainer: 'swal-wide',
-			popup: 'swal-wide'
-		}
-	});
-}
 
 function trunc(string, length = 500) {
 	const array = string.split('').splice(0, length);
