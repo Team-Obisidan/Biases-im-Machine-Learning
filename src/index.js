bb26d5aa0dad4ec67c55885c9cbfee48cb865348
Lukas Fruntke <lukas@fruntke.tech>
2021-06-09 13:44:22 +0200

    Add Roboto Font

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@fontsource/roboto'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'animate.css/animate.css';
 import './index.css';
