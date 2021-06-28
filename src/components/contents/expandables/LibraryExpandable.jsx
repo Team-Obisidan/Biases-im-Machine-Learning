6993807bf2db341f8e900037b0f10d57af52c9e3
Lukas Fruntke <lukas@fruntke.tech>
2021-06-28 14:27:50 +0200

    Feature/23 finish content (#24)
    * add library
    
    * add library
    
    Co-authored-by: Tom Dott <tomdott97@gmail.com>

--- src/components/contents/expandables/LibraryExpandable.jsx
+++ src/components/contents/expandables/LibraryExpandable.jsx
@@ -10,10 +10,19 @@ export const LibraryExpandable = () => {
 			<FontAwesomeIcon icon={faBook} size="4x"/>
 			<span/>
 			<div className="text-left">
+				<p><span className="font-weight-bold">Adorno, M./Houdeau, D./Schauf, T./Campbell, P. C./Werling, C./Dodt, A. (2020):</span> Machine Learning in der Anwendung, in: Digitale Welt, 4. Jg., Nr. 3, S. 67–75</p>
+				
+				<p><span className="font-weight-bold">Angwin, J./Larson, J./Mattu, S./Kirchner, L. (2016):</span> Machine Bias. There’s software used across the country to predict future criminals. And it’s biased against blacks., https://www.propublica.org/article/machine-bias-risk-assessments-in-criminal-sentencing, Abruf: 25.05.2021</p>
+				<p><span className="font-weight-bold">Arora, A. (2018):</span> Anwendungsgebiete für Machine Learning, https://www.agile-im.de/2018/02/08/anwendungsgebiete-fuer-machine-learning/, Abruf: 21.06.2021</p>
+				
+				<p><span className="font-weight-bold">DeVries, T./Misra, I./Wang, C./van der Maaten, L. (2019):</span> Does Object Recognition Work for Everyone? In: Computer Vision Foundation / IEEE (ed.): IEEE Conference on Computer Vision and Pattern Recognition Workshops, CVPR Workshops 2019: Long Beach, CA, USA, S. 52–59</p>
 				<p><span className="font-weight-bold">Hagendorff, T. (2019):</span> Maschinelles Lernen und Diskriminierung: Probleme und Lösungsansätze, in: Österreichische Zeitschrift für Soziologie, 44. Jg., Nr. S1, S. 53–66</p>
 				<p><span className="font-weight-bold">Kagian, A./Dror, G./Leyvand, T./Meipjson, I./Cohen-Or, D./Ruppin, E. (2008):</span> A machine learning predictor of facial attractiveness reveapng human-pke psychophysical biases, in: Vision research, 48. Jg., Nr. 2, S. 235–243</p>
 				<p><span className="font-weight-bold">Kersting, K./Tresp, V. (2019):</span> Maschinelles und Tiefes Lernen: Der Motor für „KI made in Germany“, in: Digitale Welt, 3. Jg., Nr. 4, S. 32–34</p>
+				<p><span className="font-weight-bold">Liu, H./Gegov, A./Cocea, M. (2016):</span> Nature and biology inspired approach of classification towards reduction of bias in machine learning, In: 2016 International Conference on Machine Learning and Cybernetics (ICMLC), S. 588–593</p>
 				<p><span className="font-weight-bold">Mehrabi, N./Morstatter, F./Saxena, N./Lerman, K./Galstyan, A. (2019):</span> A Survey on Bias and Fairness in Machine Learning, http://arxiv.org/pdf/1908.09635v2, Abruf: 10.06.2021</p>
+				<p><span className="font-weight-bold">Northpoint Inc. (2021):</span> Practitioners-Guide-to-COMPAS-Core, https://www.documentcloud.org/documents/2840784-Practitioner-s-Guide-to-COMPAS-Core.html#document/p30/a296482, Abruf: 21.06.2021</p>
+				<p><span className="font-weight-bold">Oppenheim, M. (2018):</span> Amazon scraps 'sexist AI' recruitment tool, https://www.independent.co.uk/life-style/gadgets-and-tech/amazon-ai-sexist-recruitment-tool-algorithm-a8579161.html, Abruf: 21.06.2021</p>
 				<p><span className="font-weight-bold">Schäfers, B. (2018):</span> Gesellschaft, in: Grundbegriffe der Soziologie, (Hrsg.: Kopp, J./Steinbach, A.), Wiesbaden: Springer VS, S. 141–145</p>
 				<p><span className="font-weight-bold">Suresh H. / Guttag J. V. (2020):</span> A Framwork for Unterstanding Unintended Consequences of Machine Learning, https://arxiv.org/abs/1901.10002, Abruf: 10.06.2021</p>
 				<p><span className="font-weight-bold">Welsch, A./Eitle, V./Buxmann, P. (2018):</span> Maschinelles Lernen, in: HMD Praxis der Wirtschaftsinformatik, 55. Jg., Nr. 2, S. 366–382</p>
--- src/components/contents/mitigations/MitiBasicsComponent.jsx
+++ src/components/contents/mitigations/MitiBasicsComponent.jsx
@@ -1,20 +1,88 @@
+import { Footnotes } from '@team-obisidan/react-footnotes';
+import { getFooter } from 'components/GetFooter.jsx';
 import React, {Component} from 'react';
 import {TextExpandable} from '../expandables/TextExpandable.jsx';
 
 export class MitiBasicsComponent extends Component {
 	render() {
 		return (
-			<TextExpandable title="Organisationell" length="250">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.    Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.   Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.   Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.   At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.
+			<TextExpandable title="Grundlagen der Mitigation von Biases" length="250">
+				<Footnotes>
+								{({Footnote, getFootnotes}) => (
+									<div style={{textAlign: 'left'}}>
 
-				Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus.
+										<p>
+										Die Mitigation (engl. für Abmilderung) von Biases (engl. für Vorurteile, Voreingenommenheiten, Neigungen) verfolgt das Ziel, innerhalb von Machine Learning Algorithmen Verzerrungen gegenüber unterprivilegierten Gruppen zu entzerren und dabei faire Vorhersagen zu treffen, ohne die Klassifizierungsgenauigkeit und die Performance eines Algorithmus zu beeinflussen. 
+										Die Mitigation beeinflusst dabei alle Personengruppen, um ein faires Leben miteinander zu ermöglichen. Die Erkennung solcher Biases und die darauffolgende Mitigation ist in vielen Fällen schwer zu identifizieren und zu lösen.<Footnote i={1} desc="Vgl. Bentivegna 2020"/>   
+										<br />
+										<br />
+										Für ein näheres Verständnis werden im Folgenden grundlegende Konzepte dargestellt, wie Vorurteile innerhalb des Machine Learnings abgemildert werden können. Dabei soll betrachtet werden, wie ein Vorurteil entsteht, bzw. als solches klassifiziert werden kann und welche kognitiven Prinzipien dahinterstehen.
+										</p>
+										
 
-				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+										<p>
+										<br />
+										<span className="font-weight-bold">Fairness im Machine Learning</span>
+										<br />
+										<br />
+										Die Verzerrung von Algorithmen ist ein allgegenwärtiger Zustand, obwohl Entwickler solche Verzerrungen - die z.B. durch Diskriminierung entstehen - in der Programmierung nicht vorgesehen haben und Algorithmen nicht darauf ausgerichtet sind.<Footnote i={2} desc="Vgl. Hajian/Bonchi/Castillo 2016, S. 2125"/>    
+										{" "}
+										Da solche Machine Learning Systeme allgegenwärtig innerhalb des alltäglichen Umfelds sind, muss das Prinzip von Fairness in die Konzeption und Entwicklung von Machine Learning Algorithmen aufgenommen werden.<Footnote i={3} desc="Vgl. Mehrabi u. a. 2019, S. 1"/>   
+										<br />
+										<br />
+										Nach Mehrabi u. a. (2019) ist Fairness im Kontext der Entscheidungsfindung die Abwesenheit jeglicher Voreingenommenheit oder Bevorzugung einer Person oder einer Gruppe aufgrund ihrer angeborenen oder erworbenen Eigenschaften.<Footnote i={4} desc="Vgl. Mehrabi u. a. 2019, S. 2"/> 
+										{" "}
+										Ein Machine Learning Modell, das die Fairness berücksichtigt – auch als Fairness-Aware Machine Learning (FAML) gängig, wird in unterschiedlichen Bereichen wie bspw. innerhalb der Rechtsprechung oder der Medizin benötigt. Neben dem Training eines solchen FAML-Systems, kann Fairness durch die Mitigation von Biases ermöglicht werden.<Footnote i={5} desc="Vgl. Das u. a. 2020, S. 1"/> 
+										<br />
+										<br />
+										Biases können in zwei Kategorien eingeteilt werden – User Biases (engl. für Anwender-Verzerrungen) und Content Biases (engl. für inhaltliche Verzerrungen). Bei einem User Bias handelt es sich um die Verzerrung eines Anwenders, der Informationen erhält.  Es wird angenommen, dass Attribute, die einen Anwender maßgeblich charakterisieren – so z.B. das Geschlecht oder die ethnische Herkunft – gesichert sind. Ein User Bias liegt dann vor, wenn solche Attribute das <Footnote i={6} desc="Vgl. Pitoura u. a. 2018, S. 17"/> 
 
-				Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+										</p>
 
-				Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+										<p>
+										<br />
+										<span className="font-weight-bold">Kausalität und Korrelation im Machine Learning</span>
+										<br />
+										<br />
+										Eine Publikation aus 2016 stellt dar, wie der Algorithmus Correctional Offender Management Profiling for Alternative Sanctions (COMPAS), welcher zu damaliger Zeit von unterschiedlichen Bundesstaaten der USA zur Rechtsprechung genutzt wurde, rassistisch voreingenommen ist.<Footnote i={7} desc="Vgl. Angwin u. a. 2016"/>   
+										{" "}
+										Der Algorithmus schätzt demnach, dass eine Person mit schwarzer Hautfarbe eine höhere Wahrscheinlichkeit hat erneut strafrechtlich auffällig zu werden, als eine Person mit weißer Hautfarbe und den exakt gleichen Hintergrundparametern.<Footnote i={8} desc="Vgl. Lovejoy 2020"/>  
+										<br />
+										<br />
+										Das Problem des Algorithmus stellt die Verwechslung von Kausalität und Korrelation dar. Somit werden Muster von Verbrechen, welche in der Vergangenheit aufgetreten sind (Korrelation) damit verwechselt, dass die Hautfarbe einer Person die Wahrscheinlichkeit erhöht ein Verbrechen zu begehen (Kausalität).<Footnote i={9} desc="Vgl. Lovejoy 2020"/>  
+										<br />
+										<br />
+										Die Kausalität wird innerhalb der philosophischen Diskussion „oft als eine spezifische Beziehung zwischen Ereignissen betrachtet.“.<Footnote i={10} desc="Kühnel/Dingelstedt 2014, S. 1018"/>  
+										{" "}
+										Die empirische Sozialforschung betrachtet die Kausalität als Frage danach, warum das Ergebnis B aufgetreten ist und sucht hierbei den Auslöser A. Spezifiziert soll hier die Beziehung zwischen A und B identifiziert werden und die Frage danach, ob das Ereignis A das Ergebnis B auslöst.<Footnote i={11} desc="Vgl. Kühnel/Dingelstedt 2014, S. 1018"/> 
+										<br />
+										<br />
+										Ein Beispiel für den Unterschied von Kausalität und Korrelation stellt ein heißer Sommertag dar, an dem die Sonne stark scheint. Dieser Sommertag hat als Kausalität, dass sich eine Person wegen der Hitze eine Kugel Eis kauft. Eine weitere Kausalität ist, dass diese Person durch die starke Sonneneinstrahlung einen Sonnenbrand bekommt. Bei der Beziehung zwischen Sonnenbrand und der Kugel Eis handelt es sich um eine Korrelation. Die Beziehung zwischen Sonnenbrand und Eis ist somit kein Kausalzusammenhang.<Footnote i={12} desc="Vgl. Figueroa 2019"/>   
+										<br />
+										<br />
+										Machine Learning ist sehr gut darin, Beziehungen in komplexen und großen Datenmengen zu identifizieren. Das Problem dabei besteht darin, dass es sich bei jenen Beziehungen um Korrelationen und nicht um Kausalitäten handelt, weshalb auch das ursprüngliche Problem des COMPAS-Algorithmus in den USA aufgetreten ist und der Einsatz im strafrechtlichen Prozess letztendlich gescheitert ist.<Footnote i={13} desc="Vgl. Lovejoy 2020"/> 
 
-				Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+										</p>
+
+										<p>
+										<br />
+										<span className="font-weight-bold">Zukunft von Machine Learning</span>
+										<br />
+										<br />
+										Die Zukunft des Machine Learning ist es, solchen Algorithmen das Prinzip der Kausalität beizubringen, um zuvor erläuterten Problemen wie denen des COMPAS-Algorithmus entgegenwirken zu können. Nach Lovejoy bildet ein Paper von Babylon Heath einen Ansatz, wie das umgesetzt werden kann.<Footnote i={14} desc="Vgl. Lovejoy 2020"/> 
+										{" "}
+										Hierbei wird die Diagnostik von Krankheiten näher betrachtet. Ein Arzt versucht die Symptome durch die Determinierung der Krankheit eines Patienten oder einer Patientin zu erklären. Bestehendes maschinelles Lernen identifiziert Krankheiten assoziativ, indem welche Krankheiten mit welchen bestimmten Symptomen korrelieren. Dadurch können ernsthafte Fehldiagnostiken attestiert werden.<Footnote i={15} desc="Vgl. Richens/Lee/Johri 2020, S. 1"/>
+										<br /> 
+										<br />
+										Damit die Anzahl von Fehldiagnosen durch Machine Learning reduziert werden, soll in dem erläuterten Beispiel ein kausales Modell gewählt werden, indem die Behandlung einer Disease (engl. für Krankheit) D, zur Abmilderung eines Symptoms S führt. Als weitere Variable werden andere Außenfaktoren, z.B. eine genetische Auslegung als Variable R berücksichtigt.<Footnote i={16} desc="Vgl. Richens/Lee/Johri 2020, S. 3 ff."/> 
+										Dabei wird betrachtet, welche Symptome verschwinden, wenn eine spezifische Krankheit behandelt wird und welche dieser Symptome weiterhin erhalten bleiben, wenn keine Behandlung durchgeführt wird. 
+
+										</p>
+
+										{getFooter(getFootnotes)}
+									</div>
+								)}
+				</Footnotes>
 			</TextExpandable>
 		);
 	}
